075435eb428769437ae0ee436fb58c6091e936e1
brena-odwyer <odwyer.brena@gmail.com>
2022-06-07 16:15:13 -0300

    get por nome funcionando mais ou menos; create (post) funcionando

--- para-o-lar/src/controllers/iniciativasController.js
+++ para-o-lar/src/controllers/iniciativasController.js
@@ -7,6 +7,69 @@ const allIniciativas = (req, res) => {
     })
 }
 
+const findById = (req, res) => {
+    try {
+        const getId = req.params.id
+        const findId = iniciativasModel.find(iniciativa => iniciativa.id == getId)
+        if (!findId) {
+            throw new Error ("Id não encontrado.")
+        }
+        res.status(200).json({
+            "message": "Inciativa encontrada com sucesso",
+            findId
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+        
+    }
+}
+
+const findByName = (req, res) => {
+    try {
+        const getName = req.params.nome
+        const findName = iniciativasModel.filter(iniciativa => iniciativa.nome == getName)
+        if (!findName) {
+            throw new Error ("Nome não encontrado.")
+        }
+        res.status(200).json({
+            "message": "Iniciativa encontrada",
+            findName
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+const createIniciativa = (req, res) => {
+    try {
+        const bodyRequest = req.body
+
+        let newIniciativa = {
+            id: (iniciativasModel.length)+1,
+            nome: bodyRequest.nome,
+            temas: bodyRequest.temas,
+            site: bodyRequest.site,
+            descricao: bodyRequest.descricao
+        }
+
+        iniciativasModel.push(newIniciativa)
+
+        res.status(201).send({
+            "message": "Iniciativa cadastrada com sucesso",
+            newIniciativa
+        })
+        
+    } catch (error) {
+        response.status(500).json({
+            message: error.message
+        })
+    }
+}
 
 
 
@@ -17,4 +80,7 @@ const allIniciativas = (req, res) => {
 
 module.exports = {
     allIniciativas,
+    findById,
+    findByName,
+    createIniciativa
 }
--- para-o-lar/src/routes/iniciativasRoutes.js
+++ para-o-lar/src/routes/iniciativasRoutes.js
@@ -5,5 +5,7 @@ const controller = require("../controllers/iniciativasController")
 const routes = express.Router()
 
 routes.get("/todas", controller.allIniciativas)
-
+routes.get("/filter/:id", controller.findById)
+routes.get("/filterName", controller.findByName)
+routes.post("/create", controller.createIniciativa)
 module.exports = routes
