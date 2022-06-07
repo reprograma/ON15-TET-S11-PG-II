0d3fd112ff815ee60579f943874b578bbe83d81f
Alice_Lins <alicelins17@yahoo.com>
2022-06-07 16:01:53 -0300

    Minhas anotaçoes

--- cadastrando-hospital/src/controller/pacienteController.js
+++ cadastrando-hospital/src/controller/pacienteController.js
@@ -2,6 +2,7 @@
 
 // importar o json /banco de dados
 
+
 const { response } = require("../app")
 const pacientesModel = require("../models/pacienteModel.json")
 //Precisamos listar os pacientes desse hospital (GET)
@@ -11,7 +12,52 @@ const todosPacientes = (request, response) => {
         pacientesModel
     })
 }
-// Precisamos listar por nome , nome social e id(GET)
+// Precisamos listar por  id(GET)
+const buscarPorId = (request, response) =>{
+    try {
+        //identificar o id do parametro
+        const chamarId = request.params.id
+        const acharId = pacientesModel.find(paciente => paciente.pacienteId == chamarId)
+        if(!acharId){ throw new error("id nao encontrado")
+    }
+    response.status(200).json(acharId)
+    } catch (error) {
+       response.status(500).json({
+           mensage: error.mensage
+           
+       })
+       console.log(error)
+    }
+}
+
+//listar por nome, se tiver nome social
+const buscarPorNome = (request, response) =>{
+    try {
+        const trazerNome = request.query.nome.tolowerCase
+        const encontrarNome = pacientesModel.filter(paciente => {
+        if(paciente.nomeSocial){
+            return paciente.nomeSocial.toLowerCase().includes(trazerNome)
+        }
+           return paciente.nome.toLowerCase().includes(trazerNome)
+        })
+    
+       if(encontrarNome.length == 0){
+        throw new error("Nome nao encontrado")
+
+    }
+
+    response.status(200).json({
+        "mensagem": "Paciente encontrado",
+        encontrarNome
+    })
+
+    } catch (error) {
+       response.status(500).json({
+           menssage: error.menssage
+       }) 
+       console.log(error)
+    }
+}
 //Cadastrar no sistema um paciente (POST)
 // Atualizar o cadastro de um paciente no sistema(PUT)
 //Deletar o cadastro de um paciente (DELETE)
@@ -20,4 +66,6 @@ const todosPacientes = (request, response) => {
 //Exportar as variaveis do controller
 module.exports = {
     todosPacientes,
+    buscarPorId,
+    buscarPorNome
 }
