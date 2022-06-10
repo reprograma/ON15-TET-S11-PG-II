23de76b9ef6f12b3396117f03641b22d10a8e5a8
gabskovisk <gabskovisk@gmail.com>
2022-06-10 17:58:28 -0300

    atualizar exercicio

--- para-o-lar/src/routes/feiraRoutes.js
+++ para-o-lar/src/routes/feiraRoutes.js
@@ -1,9 +1,19 @@
-const express = require("express")
+const express = require("express");
 
-const controller = require("../controller/feiraController")
+const controller = require("../controller/feiraController");
 
-const routes = express.Router()
+const routes = express.Router();
 
-routes.get("/all", controller.allBusiness)
+routes.get("/all", controller.allBusiness);
+
+routes.get("/filter/:id", controller.findById);
+
+routes.get("/name", controller.findByName);
+
+routes.post("/register", controller.registerBusiness);
+
+routes.put("/update/:id", controller.updateBusiness);
+
+routes.delete("/delete/:id", controller.deleteBusiness);
 
 module.exports = routes
