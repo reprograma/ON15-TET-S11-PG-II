89f946223965ca1ad3b8ec588cf5b0531047cf12
Maria Fernanda Tavares <mariaffernandatavares@gmail.com>
2022-06-09 23:18:49 -0300

    Subindo rotas das fichas de academia

--- para-o-lar/src/controller/academiaController.js
+++ para-o-lar/src/controller/academiaController.js
@@ -4,52 +4,58 @@ const fichasAcademia = require('../models/fichaAcademiaModel.json')
 
 /* Rotas para dados de cliente*/
 
-const todosClientes = (request,response)=>{
+const todosClientes = (request, response) => {
     try {
-        if(clienteModel.length == 0) throw new Error('Ocorreu um erro, não foi possivel trazer as lista de clientes')
+        if (clienteModel.length == 0) throw new Error('Ocorreu um erro, não foi possivel trazer as lista de clientes')
         response.status(200).send(clienteModel)
     } catch (error) {
-        response.status(500).json({message:error.message})
+        response.status(500).json({
+            message: error.message
+        })
     }
 }
 
-const clientePorId = (request,response)=>{
+const clientePorId = (request, response) => {
     try {
-        const idCliente  = request.params.id
-        if(idCliente == null || idCliente == undefined)throw new Error('Ocorreu um erro,não foi digitado o ID do cliente que deseja encontrar')
-        const clienteEncontrado = clienteModel.find(cliente => cliente.clienteId == idCliente )
-        if(!clienteEncontrado)throw new Error('Ocorreu um erro,não foi encontrado nenhum cliente com id digitado')
+        const idCliente = request.params.id
+        if (idCliente == null || idCliente == undefined) throw new Error('Ocorreu um erro,não foi digitado o ID do cliente que deseja encontrar')
+        const clienteEncontrado = clienteModel.find(cliente => cliente.clienteId == idCliente)
+        if (!clienteEncontrado) throw new Error('Ocorreu um erro,não foi encontrado nenhum cliente com id digitado')
         response.status(200).send(clienteEncontrado)
-        
+
     } catch (error) {
-        response.status(500).json({message:error.message})
+        response.status(500).json({
+            message: error.message
+        })
     }
 }
 
 
-const clientePorNome =(request,response)=>{
+const clientePorNome = (request, response) => {
     try {
-        const nomeCliente  = request.query.nome
-        if(nomeCliente == null || nomeCliente == undefined)throw new Error('Ocorreu um erro,não foi digitado o nome do cliente que deseja encontrar')
-        const clienteEncontrado = clienteModel.filter(cliente => cliente.nome.toLocaleLowerCase().includes(nomeCliente.toLocaleLowerCase()) )
-        if(clienteEncontrado.length == 0)throw new Error('Ocorreu um erro,não foi encontrado nenhum cliente com nome digitado')
+        const nomeCliente = request.query.nome
+        if (nomeCliente == null || nomeCliente == undefined) throw new Error('Ocorreu um erro,não foi digitado o nome do cliente que deseja encontrar')
+        const clienteEncontrado = clienteModel.filter(cliente => cliente.nome.toLocaleLowerCase().includes(nomeCliente.toLocaleLowerCase()))
+        if (clienteEncontrado.length == 0) throw new Error('Ocorreu um erro,não foi encontrado nenhum cliente com nome digitado')
         response.status(200).send(clienteEncontrado)
-        
+
     } catch (error) {
-        response.status(500).json({message:error.message})
+        response.status(500).json({
+            message: error.message
+        })
     }
 }
 
 
-const cadastrarCliente = (request,response)=>{
+const cadastrarCliente = (request, response) => {
     try {
-        const novocliente  = request.body
-        if(novocliente == null || novocliente == undefined)throw new Error('Ocorreu um erro,não foi enviado os dados do novo cliente que deseja cadastrar')
-        if(novocliente.nome == undefined && novocliente.telefone == undefined )throw new Error('Ocorreu um erro,não foi enviado o nome e o telefone do novo cliente,por favor inserir dados obrigatorios')
-      
+        const novocliente = request.body
+        if (novocliente == null || novocliente == undefined) throw new Error('Ocorreu um erro,não foi enviado os dados do novo cliente que deseja cadastrar')
+        if (novocliente.nome == undefined && novocliente.telefone == undefined) throw new Error('Ocorreu um erro,não foi enviado o nome e o telefone do novo cliente,por favor inserir dados obrigatorios')
+
         const clienteNovo = {
-            id: clienteModel.length+1,
-            nome: novocliente.nome ,
+            id: clienteModel.length + 1,
+            nome: novocliente.nome,
             idade: novocliente.idade,
             endereco: novocliente.endereco,
             telefone: novocliente.telefone,
@@ -57,20 +63,22 @@ const cadastrarCliente = (request,response)=>{
         }
         clienteModel.push(clienteNovo)
         response.status(200).send(clienteNovo)
-        
+
     } catch (error) {
-        response.status(500).json({message:error.message})
+        response.status(500).json({
+            message: error.message
+        })
     }
 }
 
-const atualizarCadastro = (request,response)=>{
+const atualizarCadastro = (request, response) => {
     try {
         const idCliente = request.params.id
         const atualizarCliente = request.body
-        if(idCliente == null || idCliente == undefined)throw new Error('Ocorreu um erro,não foi digitado o ID do cliente que deseja atualizar')
-        const clienteEncontrado = clienteModel.find(cliente => cliente.clienteId == idCliente )
-        if(!clienteEncontrado)throw new Error('Ocorreu um erro,não foi encontrado nenhum cliente com id digitado')
-        for (let dado in atualizarCliente ){
+        if (idCliente == null || idCliente == undefined) throw new Error('Ocorreu um erro,não foi digitado o ID do cliente que deseja atualizar')
+        const clienteEncontrado = clienteModel.find(cliente => cliente.clienteId == idCliente)
+        if (!clienteEncontrado) throw new Error('Ocorreu um erro,não foi encontrado nenhum cliente com id digitado')
+        for (let dado in atualizarCliente) {
             console.log(dado)
             clienteEncontrado[dado] = atualizarCliente[dado]
         }
@@ -78,30 +86,74 @@ const atualizarCadastro = (request,response)=>{
             "mensagem": "Cliente atualizado",
             clienteEncontrado
         }])
-        
+
     } catch (error) {
-        response.status(500).json({message:error.message})
+        response.status(500).json({
+            message: error.message
+        })
     }
 }
 
-const excluirCliente = (request,response)=>{
+const excluirCliente = (request, response) => {
     try {
-        const idCliente  = request.params.id
-        if(idCliente == null || idCliente == undefined)throw new Error('Ocorreu um erro,não foi digitado o ID do cliente que deseja atualizar')
-        const clienteEncontrado = clienteModel.find(cliente => cliente.clienteId == idCliente )
-        if(!clienteEncontrado)throw new Error('Ocorreu um erro,não foi encontrado nenhum cliente com id digitado')
+        const idCliente = request.params.id
+        if (idCliente == null || idCliente == undefined) throw new Error('Ocorreu um erro,não foi digitado o ID do cliente que deseja atualizar')
+        const clienteEncontrado = clienteModel.find(cliente => cliente.clienteId == idCliente)
+        if (!clienteEncontrado) throw new Error('Ocorreu um erro,não foi encontrado nenhum cliente com id digitado')
         const indice = clienteModel.indexOf(clienteEncontrado)
-        clienteModel.splice(indice,1)
+        clienteModel.splice(indice, 1)
         response.status(200).send("Cliente excluido com sucesso")
-        
+
     } catch (error) {
-        response.status(500).json({message:error.message})
+        response.status(500).json({
+            message: error.message
+        })
     }
 }
 
 /* Rotas ligando cliente e ficha */
 
 
+const fichaPoridCliente = (request, response) => {
+    try {
+        const idCliente = request.params.id
+        if (idCliente == null || idCliente == undefined) throw new Error('Ocorreu um erro,não foi digitado o ID do cliente')
+        const fichas = fichasAcademia.filter(ficha => ficha.clienteid == idCliente)
+        if (fichas.length == 0) throw new Error('Ocorreu um erro,não foi encontrado nenhuma ficha desse cliente do id digitado')
+        for (let item of fichas) {
+            item.clienteid = clienteModel.find(cliente => cliente.clienteId == idCliente)
+        }
+
+        response.status(200).send(fichas)
+
+    } catch (error) {
+        response.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+
+const cadastrarFicha = (request, response) => {
+    try {
+        const novaFicha = request.body
+        if (novaFicha.clienteid == null || novaFicha.clienteid == undefined) throw new Error('Ocorreu um erro,não foi digitado o ID do cliente que deseja cadastrar uma nova ficha')
+        const cliente = clienteModel.find(cliente => cliente.clienteId == novaFicha.clienteid)
+        if (!cliente) throw new Error('Ocorreu um erro,não existe nenhum cliente com o id enviado nos dados')
+        const fichaNova = {
+            fichaId: fichasAcademia.length + 1,
+            clienteid: novaFicha.clienteid,
+            exercicios: novaFicha.exercicios
+        }
+        fichasAcademia.push(fichaNova)
+
+        response.status(200).send(fichaNova)
+    } catch (error) {
+        response.status(500).json({
+            message: error.message
+        })
+    }
+}
 
 
 
@@ -114,19 +166,13 @@ const excluirCliente = (request,response)=>{
 
 
 
-
-
-
-
-
-
-
-
-module.exports={
-todosClientes,
-clientePorId,
-clientePorNome,
-cadastrarCliente,
-atualizarCadastro,
-excluirCliente
+module.exports = {
+    todosClientes,
+    clientePorId,
+    clientePorNome,
+    cadastrarCliente,
+    atualizarCadastro,
+    excluirCliente,
+    fichaPoridCliente,
+    cadastrarFicha
 }
